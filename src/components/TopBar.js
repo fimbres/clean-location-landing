3751f65d424e4f5bc144ef874cd2cd0273ebc7f3
Isaac Fimbres <fimbresisaac23@gmail.com>
2022-11-12 15:39:22 -0800

    comment

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,18 +4,18 @@ import LogoImg from '../images/icon.png';
 
 const TopBar = () => {
 
-  const [scrollHeight, setScrollHeight] = useState(0);
+  // const [scrollHeight, setScrollHeight] = useState(0);
 
-  const isBrowser = typeof('window' !== "undefined");
+  // const isBrowser = typeof('window' !== "undefined");
 
-  function handleScroll(){
-    const position = window.pageYOffset;
-    setScrollHeight(position);
-  }
+  // function handleScroll(){
+  //   const position = window.pageYOffset;
+  //   setScrollHeight(position);
+  // }
 
-  if(isBrowser){
-    window.addEventListener('scroll',handleScroll);
-  }
+  // if(isBrowser){
+  //   window.addEventListener('scroll',handleScroll);
+  // }
 
   return (
     <div className={`flex w-full transition-all ease-in-out h-28 justify-start items-center px-6 fixed top-0 right-0 left-0 ${scrollHeight === 0 ? 'bg-transparent' : 'bg-zinc-800'}`}>

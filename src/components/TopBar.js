61f0364e0ad2fb50228e1bedd46466181c0add1c
Isaac Fimbres <fimbresisaac23@gmail.com>
2022-11-12 15:41:30 -0800

    fix

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -18,7 +18,7 @@ const TopBar = () => {
   // }
 
   return (
-    <div className={`flex w-full transition-all ease-in-out h-28 justify-start items-center px-6 fixed top-0 right-0 left-0 ${scrollHeight === 0 ? 'bg-transparent' : 'bg-zinc-800'}`}>
+    <div className={`flex w-full transition-all ease-in-out h-28 justify-start items-center px-6 fixed top-0 right-0 left-0 ${false ? 'bg-transparent' : 'bg-zinc-800'}`}>
       <img src={LogoImg} className='w-16 w-16 cover-mode' alt='Logo'/>
       <div className='font-semibold text-white text-2xl ml-6'>Clean Location</div>
     </div>
